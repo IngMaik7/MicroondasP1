2b01959419103610ad8e3a56f714cda851810818
Michael Ramirez <michaelrp2000@gmail.com>
2025-05-28 12:12:42 -0500

    V.16

--- programa.js
+++ programa.js
@@ -7,4 +7,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker = L.marker([4.639386, -74.082412]).addTo(map);
 
+async function CargarPuntos() {
+  var miArchivo = await fetch("microonda.geojson");
+  var datos = await miArchivo.json();
 
+  let listaFeatures = datos["features"];
+
+  for (let i = 0; i < listaFeatures.length; i++) {
+        console.log(listaFeatures[i]);
+
+        // Obtener coordenadas y cambiarlas de [lng, lat] a [lat, lng]
+        let coords = listaFeatures[i]["geometry"]["coordinates"];
+        let misCoordenadas = [coords[1], coords[0]];
+
+        var miMarcador = L.marker(misCoordenadas);
+        miMarcador.addTo(map);
+    }
+
+}
+CargarPuntos();
